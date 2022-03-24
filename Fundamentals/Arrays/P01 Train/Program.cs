2682e75025bdc624fd7a986b5ffead4724d5695a
V.Kirilov <102161443+v-kirilov@users.noreply.github.com>
2022-03-24 22:52:31 +0200

    Add files via upload

---
+++ Fundamentals/Arrays/P01 Train/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Linq;
+
+namespace _1._Train
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int wagons = int.Parse(Console.ReadLine());
+
+            int[] arr = new int[wagons];
+
+            for (int i = 0; i < arr.Length; i++)
+            {
+                arr[i] = int.Parse(Console.ReadLine());
+            }
+
+            int sum = 0;
+            foreach (var item in arr)
+            {
+                Console.Write($"{item} ");
+                sum+=item;
+            }
+            Console.WriteLine();
+            Console.WriteLine(sum);
+
+            //Console.WriteLine(String.Join(" ",arr));  
+        }
+    }
+}
