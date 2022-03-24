3ac09ca6355e7cb1680323f1e0e20a2f372ed498
v-kirilov <v_kirilov87@protonmail.ch>
2022-03-24 23:20:11 +0200

    Adding Problem 02

---
+++ Fundamentals/Arrays/P02 Common Elements/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Linq;
+
+namespace _2._Common_Elements
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] firstArr = Console.ReadLine()
+                .Split(' ',StringSplitOptions.RemoveEmptyEntries)
+                .ToArray();
+
+            string[] secondArr = Console.ReadLine()
+                .Split(' ', StringSplitOptions.RemoveEmptyEntries)
+                .ToArray();
+
+            string[] compareArray = new string[firstArr.Length];
+
+            for (int i = 0; i < secondArr.Length; i++)
+            {
+                for (int j = 0; j < firstArr.Length; j++)
+                {
+                    if (firstArr[j]==secondArr[i])
+                    {
+                        compareArray[i] = secondArr[i];
+                    }
+                }
+            }
+
+            compareArray = string.Join(" ", compareArray)
+                .Split(new string[] { " " }, StringSplitOptions.RemoveEmptyEntries); // Презаписва масива като трие празните членове на масива
+            foreach (var item in compareArray)
+            {
+                Console.Write($"{item} ");
+            }
+        }
+    }
+}
