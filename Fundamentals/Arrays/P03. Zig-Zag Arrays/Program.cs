f292d484670274450b534633150e5e6a292bb4dc
v-kirilov <v_kirilov87@protonmail.ch>
2022-03-24 23:20:55 +0200

    Adding problem 03

---
+++ Fundamentals/Arrays/P03. Zig-Zag Arrays/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Linq;
+
+namespace P03._Zig_Zag_Arrays
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+
+            int[] arrOdd = new int[n];
+            int[] arrEven = new int[n];
+
+            for (int i = 1; i <= n; i++)
+            {
+                int[] input = Console.ReadLine()
+                    .Split(' ', StringSplitOptions.RemoveEmptyEntries)
+                    .Select(int.Parse)
+                    .ToArray();
+
+                if (i % 2 != 0)
+                {
+                    arrOdd[i - 1] = input[0];
+                    arrEven[i - 1] = input[1];
+                }
+                else
+                {
+                    arrEven[i - 1] = input[0];
+                    arrOdd[i - 1] = input[1];
+                }
+            }
+
+            Console.Write(String.Join(" ", arrOdd));
+            Console.WriteLine();
+            Console.Write(String.Join(" ", arrEven));
+
+        }
+    }
+}
