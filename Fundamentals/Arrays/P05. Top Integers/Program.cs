aac49c47534ca54cf6fc306963ccc5616cc6fdb8
v-kirilov <v_kirilov87@protonmail.ch>
2022-03-25 17:18:31 +0200

    Problem 05 Top Integers

---
+++ Fundamentals/Arrays/P05. Top Integers/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Linq;
+
+namespace P05._Top_Integers
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] numberArr = Console.ReadLine()
+                .Split(' ', StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse)
+                .ToArray();
+
+
+            for (int i = 0; i < numberArr.Length; i++)
+            {
+                bool isGreater = true;
+
+                for (int j = i+1; j < numberArr.Length; j++)
+                {
+                    if (numberArr[i] <= numberArr[j])
+                    {
+                        isGreater = false;
+                    }
+
+                }
+                if (isGreater)
+                {
+                    Console.Write(numberArr[i]+" ");
+
+                }
+
+            }
+        }
+    }
+}
