f7a366ac25451739e5e837a56de99d01ec6274c1
v-kirilov <v_kirilov87@protonmail.ch>
2022-03-26 08:59:33 +0200

    Problem 7 && 8

---
+++ Fundamentals/Arrays/P07. Max Sequence of Equal Elements/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Linq;
+
+namespace P07._Max_Sequence_of_Equal_Elements
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] arr = Console.ReadLine()
+                .Split(' ', StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse)
+                .ToArray();
+            int repeatedNumber = 0;
+            int maxCounter = int.MinValue;
+
+
+            for (int i = 0; i < arr.Length; i++)
+            {
+                int currentNum = arr[i];
+                int counter = 0;
+
+                for (int j = i+1; j < arr.Length; j++)
+                {
+                    if (currentNum == arr[j])
+                    {
+                        counter++;
+                    }else
+                    {
+                        break;
+                    }
+                   
+                }
+
+                if (counter > maxCounter)
+                {
+                    maxCounter = counter;
+                    repeatedNumber = currentNum;
+                }
+            }
+
+            for (int i = 0; i <= maxCounter; i++)
+            {
+                Console.Write(repeatedNumber+" ");
+            }
+        }
+    }
+}
---
+++ Fundamentals/Arrays/P08. Magic Sum/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Linq;
+
+namespace P08._Magic_Sum
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] arr = Console.ReadLine()
+                .Split(' ', StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse)
+                .ToArray();
+            int magicSum = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < arr.Length; i++)
+            {
+                int currentNum = arr[i];
+                int currentSum = 0;
+
+                for (int j = i+1; j < arr.Length; j++)
+                {
+                    currentSum = currentNum + arr[j];
+
+                    if (currentSum == magicSum)
+                    {
+                        Console.Write(currentNum + " " + arr[j]);
+                        Console.WriteLine();
+                    }
+                }
+
+            }
+        }
+    }
+}
