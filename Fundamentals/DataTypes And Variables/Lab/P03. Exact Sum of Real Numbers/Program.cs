78e4d2047bdaba2bfe66f00daa9c863cd7379ec3
v-kirilov <v_kirilov87@protonmail.ch>
2022-03-28 16:10:00 +0300

    Problem 03

---
+++ Fundamentals/DataTypes And Variables/Lab/P03. Exact Sum of Real Numbers/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace _3._Exact_Sum_of_Real_Numbers
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int num = int.Parse(Console.ReadLine());
+            decimal sum = 0;
+
+            for (int i = 0; i < num; i++)
+            {
+                decimal input = decimal.Parse(Console.ReadLine());
+                sum += input;
+            }
+            Console.WriteLine(sum);
+        }
+    }
+}
