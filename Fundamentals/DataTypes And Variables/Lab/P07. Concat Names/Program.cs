114ac3c38c8d7264f3408bfca20fe00049f7dbb8
v-kirilov <v_kirilov87@protonmail.ch>
2022-03-28 17:13:30 +0300

    Problem 07

---
+++ Fundamentals/DataTypes And Variables/Lab/P07. Concat Names/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace _7._Concat_Names
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            char input = char.Parse(Console.ReadLine());
+
+            if (char.IsUpper(input))
+            {
+                Console.WriteLine("upper-case");
+            }
+            else if (char.IsLower(input))
+            {
+                Console.WriteLine("lower-case");
+            }
+            //char inputChar = (char)Console.Read();
+
+            //int number = Convert.ToInt32(inputChar);
+
+            //if (number>=65 && number<=90)
+            //{
+            //    Console.WriteLine("upper-case");
+            //}else
+            //{
+            //    Console.WriteLine("lower-case");
+            //}
+
+            //string a = Console.ReadLine();
+            //string b = Console.ReadLine();
+            //string c = Console.ReadLine();
+
+            //string full = a + b + c;
+
+            //Console.WriteLine(full);
+
+        }
+    }
+}
