e73d815b05485face66ce346008858c3ad4649f9
v-kirilov <v_kirilov87@protonmail.ch>
2022-03-28 16:10:37 +0300

    Problem 04

---
+++ Fundamentals/DataTypes And Variables/Lab/P04. Centuries to Minutes/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace _4._Centuries_to_Minutes
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            byte century = byte.Parse(Console.ReadLine());
+            //decimal years = decimal.Multiply(century, 1000);
+            //decimal days = decimal.Multiply(years, (decimal)365.2422);
+            //decimal hours = decimal.Multiply(days, (decimal)24);
+            //decimal minutes = decimal.Multiply(hours, (decimal)60);
+            int years = century * 100;
+            int days = (int)(years * 365.2422);
+            int hours = (days * 24);
+            long minutes = (hours * 60);
+
+            Console.WriteLine($"{century} centuries = {years} years = {days} days = {hours} hours = {minutes} minutes");
+        }
+    }
+}
