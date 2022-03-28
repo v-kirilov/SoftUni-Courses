96c7c05220de29cf9ef2f14d88e0472916f483f2
v-kirilov <v_kirilov87@protonmail.ch>
2022-03-28 16:09:43 +0300

    Problem 02

---
+++ Fundamentals/DataTypes And Variables/Lab/P02. Pounds to Dollars/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace _2._Pounds_to_Dollars
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            decimal pounds = decimal.Parse(Console.ReadLine());
+            double mul = 1.31;
+            decimal convertet = decimal.Multiply(pounds, (decimal)mul);
+
+            Console.WriteLine($"{convertet:f3}");
+        }
+    }
+}
