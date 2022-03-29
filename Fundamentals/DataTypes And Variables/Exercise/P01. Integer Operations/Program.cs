c2ed704efc1d5c957fc586d9bc33398dfcbb5d56
v-kirilov <v_kirilov87@protonmail.ch>
2022-03-29 19:40:48 +0300

    Problem 01

---
+++ Fundamentals/DataTypes And Variables/Exercise/P01. Integer Operations/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Numerics;
+
+namespace P1._Integer_Operations
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int first = int.Parse(Console.ReadLine());
+            int second = int.Parse(Console.ReadLine());
+            int third = int.Parse(Console.ReadLine());
+            int fourth = int.Parse(Console.ReadLine());
+
+            BigInteger sum = first + second;
+
+            int divide = (int)sum / third;
+
+            BigInteger multiply = divide * fourth;
+
+            Console.WriteLine($"{multiply}");
+        }
+    }
+}
