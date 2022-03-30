7a2636d795cfdd222c07855d7c72862ac158c8af
v-kirilov <v_kirilov87@protonmail.ch>
2022-03-30 19:53:15 +0300

    Problem 06

---
+++ Fundamentals/DataTypes And Variables/Exercise/P06. Triples of Latin Letters/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace P6._Triples_of_Latin_Letters
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int input = int.Parse(Console.ReadLine());
+
+            int first = 97;
+
+            for (int i = 0; i < input; i++)
+            {
+                int second = 97;
+
+
+                for (int j = 0; j < input; j++)
+                {
+                    int third = 97;
+
+                    for (int k = 0; k < input; k++)
+                    {
+                        Console.WriteLine($"{(char)(first)}{(char)(second)}{(char)(third)}");
+                        third++;
+                    }
+                    second++;
+                }
+                first++;
+            }
+        }
+    }
+}
