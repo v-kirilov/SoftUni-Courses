eb99027bf2d59b3523726a31624c1be0b59b14fb
v-kirilov <v_kirilov87@protonmail.ch>
2022-03-30 19:43:08 +0300

    Problem 04

---
+++ Fundamentals/DataTypes And Variables/Exercise/P04. Sum of Chars/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace P4._Sum_of_Chars
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            byte rows = byte.Parse(Console.ReadLine());
+            int sum = 0;
+
+            for (int i = 0; i < rows; i++)
+            {
+                char current = char.Parse(Console.ReadLine());
+                sum += current;
+            }
+
+            Console.WriteLine($"The sum equals: {sum}");
+        }
+    }
+}
