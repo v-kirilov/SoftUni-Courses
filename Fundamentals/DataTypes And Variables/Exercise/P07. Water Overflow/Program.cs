54147c6caa84f81ac53e403a5e90545e8f8d9ced
v-kirilov <v_kirilov87@protonmail.ch>
2022-03-30 19:53:44 +0300

    Problem 07

---
+++ Fundamentals/DataTypes And Variables/Exercise/P07. Water Overflow/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace P7._Water_Overflow
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            const byte capacity = 255;
+
+            int sum = 0;
+            int lines = int.Parse(Console.ReadLine());
+            int pour = 0;
+
+            for (int i = 0; i < lines; i++)
+            {
+                pour = int.Parse(Console.ReadLine());
+                if (sum + pour > capacity)
+                {
+                    Console.WriteLine("Insufficient capacity!");
+                }
+                else
+                {
+                    sum += pour;
+                }
+            }
+            if (sum <= capacity)
+            {
+                Console.WriteLine(sum);
+            }
+        }
+    }
+}
