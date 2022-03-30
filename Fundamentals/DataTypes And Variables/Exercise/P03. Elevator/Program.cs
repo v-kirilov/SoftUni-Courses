ab373d6d29d73e9711328e6c5db94dc0c8b1193d
v-kirilov <v_kirilov87@protonmail.ch>
2022-03-30 19:42:51 +0300

    Problem 03

---
+++ Fundamentals/DataTypes And Variables/Exercise/P03. Elevator/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace P3._Elevator
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int people = int.Parse(Console.ReadLine());
+            int capacity = int.Parse(Console.ReadLine());
+
+            double total = Math.Ceiling(1.0 * people / capacity);
+
+            Console.WriteLine(total);
+        }
+    }
+}
