775ee6b99f861757449944015afdbf18468cc5b9
v-kirilov <v_kirilov87@protonmail.ch>
2022-03-31 21:34:38 +0300

    Problem 10

---
+++ Fundamentals/DataTypes And Variables/Exercise/P10. Pokemon/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace P10._Pokemon
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int pokePower = int.Parse(Console.ReadLine());
+            int originalValue = pokePower;
+            int distance = int.Parse(Console.ReadLine());
+            int exhaustFactor = int.Parse(Console.ReadLine());
+            int count = 0;
+
+            while (pokePower >= distance)
+            {
+                pokePower -= distance;
+                count++;
+                if (exhaustFactor == 0)
+                {
+                    continue;
+                }
+                else if (pokePower == .5 * originalValue)
+                {
+                    pokePower /= exhaustFactor;
+                }
+            }
+
+            Console.WriteLine(pokePower);
+            Console.WriteLine(count);
+
+        }
+    }
+}
