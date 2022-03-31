ca4f1e0fe9f2a267b043b67be097401cd07992f1
v-kirilov <v_kirilov87@protonmail.ch>
2022-03-31 21:34:11 +0300

    Problem 08

---
+++ Fundamentals/DataTypes And Variables/Exercise/P8. Beer Kegs/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace P8._Beer_Kegs
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int num = int.Parse(Console.ReadLine());
+            double volume = double.MinValue;
+            double currentKeg = 0;
+            string bestModel = string.Empty;
+
+            for (int i = 0; i < num; i++)
+            {
+                string model = Console.ReadLine();
+                float radius = float.Parse(Console.ReadLine());
+                int height = int.Parse(Console.ReadLine());
+
+                currentKeg = Math.PI*radius*radius*height;
+                if (currentKeg>volume)
+                {
+                    volume = currentKeg;
+                    bestModel=model;
+                }
+            }
+
+            Console.WriteLine(bestModel);
+        }
+    }
+}
