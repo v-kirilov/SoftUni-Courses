a4052ce84b66229f0dd88cf66350b3206f9ce195
v-kirilov <v_kirilov87@protonmail.ch>
2022-03-31 21:34:24 +0300

    Problem 09

---
+++ Fundamentals/DataTypes And Variables/Exercise/P9. Spice Must Flow/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace P9._Spice_Must_Flow
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int startingYield = int.Parse(Console.ReadLine());
+            int days = 0;
+            double sum = 0;
+
+            while (startingYield >= 100)
+            {
+                sum += startingYield-26;
+                days++;
+                startingYield -= 10;
+            }
+
+            if (sum>=26)
+            {
+                sum -= 26;
+            }else
+            {
+                sum = 0;
+            }
+
+            Console.WriteLine(days);
+            Console.WriteLine(sum);
+        }
+    }
+}
