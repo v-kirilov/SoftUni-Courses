a818f7e0776655175039917a0176ccf89b1c9e3d
v-kirilov <v_kirilov87@protonmail.ch>
2022-04-01 20:58:08 +0300

    Problem 06

---
+++ Fundamentals/Intro and Basic Syntax Exercise/P06. Strong Number/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace _6._Strong_Number
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            int num = int.Parse(Console.ReadLine());
+            int firstNumber = num;
+            int sum = 0;
+            int factorial = 1;
+            while (num!=0)
+            {
+                factorial = 1;
+                int divider = num % 10;
+                for (int i = 1; i <= divider; i++)
+                {
+                    factorial *= i;
+                }
+                sum += factorial;
+                num /= 10;
+            }
+            if (sum == firstNumber)
+            {
+                Console.WriteLine("yes");
+            }else
+            {
+                Console.WriteLine("no");
+            }
+
+        }
+    }
+}
