30a5efa73622116b7d46d743afaa468ee4e4135f
v-kirilov <v_kirilov87@protonmail.ch>
2022-04-01 20:57:55 +0300

    Problem 05

---
+++ Fundamentals/Intro and Basic Syntax Exercise/P05. Login/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace _5._Login
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string userName = Console.ReadLine();
+            string guess = Console.ReadLine();
+
+            string password = "";
+            int counter = 0;
+
+            for (int i = userName.Length - 1; i >= 0; i--)
+            {
+                password += userName[i];
+            }
+
+            while (counter < 3)
+            {
+                if (guess != password)
+                {
+                    Console.WriteLine("Incorrect password. Try again.");
+                    counter++;
+                }
+                else
+                {
+                    Console.WriteLine($"User {userName} logged in.");
+                    return;
+                }
+                guess = Console.ReadLine();
+            }
+
+            if (counter == 3)
+            {
+                Console.WriteLine($"User {userName} blocked!");
+                return;
+            }
+        }
+    }
+}
