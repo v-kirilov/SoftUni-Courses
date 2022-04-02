9a8e0239e049b05799f603933b6beebc3e9db24d
v-kirilov <v_kirilov87@protonmail.ch>
2022-04-02 12:52:52 +0300

    Problem 08

---
+++ Fundamentals/Intro and Basic Syntax Exercise/P09. Padawan Equipment/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace _9._Padawan_Equipment
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double money = double.Parse(Console.ReadLine());
+            int students = int.Parse(Console.ReadLine());
+            double saberPrice = double.Parse(Console.ReadLine());
+            double beltPrice = double.Parse(Console.ReadLine());
+            double robePrice = double.Parse(Console.ReadLine());
+
+            double totalPrice = 0;
+            int check = (int)Math.Ceiling(1.1 * students);
+            int freeBelts = (students / 6);
+
+            totalPrice = check * saberPrice + beltPrice * students + robePrice * (students-freeBelts);
+
+            if (money>=totalPrice)
+            {
+                Console.WriteLine($"The money is enough - it would cost {totalPrice:f2}lv.");
+            }
+            else
+            {
+                Console.WriteLine($"John will need {totalPrice-money:f2}lv more.");
+            }
+        }
+    }
+}
