83a3601a80ffadec8d6712c4e2700c53f98d4eec
v-kirilov <v_kirilov87@protonmail.ch>
2022-04-02 12:52:40 +0300

    Problem 08

---
+++ Fundamentals/Intro and Basic Syntax Exercise/P08. Triangle of Numbers/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace _8._Triangle_of_Numbers
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int num = int.Parse(Console.ReadLine());
+
+            for (int row = 1; row <= num; row++)
+            {
+                for (int column = 1; column <= row; column++)
+                {
+                    Console.Write($"{row} ");
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
