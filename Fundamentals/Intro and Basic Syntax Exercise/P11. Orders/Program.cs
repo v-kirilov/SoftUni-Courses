df05311f6842a08606364a6a3964cb252082e076
v-kirilov <v_kirilov87@protonmail.ch>
2022-04-02 12:53:21 +0300

    Problem 11

---
+++ Fundamentals/Intro and Basic Syntax Exercise/P11. Orders/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace _11._Orders
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int orders = int.Parse(Console.ReadLine());
+            double total = 0;
+            double currentPrice = 0;
+
+            for (int i = 0; i < orders; i++)
+            {
+                double priceCapsule = double.Parse(Console.ReadLine());
+                int days = int.Parse(Console.ReadLine()); 
+                int capsules = int.Parse(Console.ReadLine());
+
+                currentPrice = (days * capsules) * priceCapsule;
+
+                Console.WriteLine($"The price for the coffee is: ${currentPrice:f2}");
+                total += currentPrice;
+            }
+
+            Console.WriteLine($"Total: ${total:f2}");
+        }
+    }
+}
