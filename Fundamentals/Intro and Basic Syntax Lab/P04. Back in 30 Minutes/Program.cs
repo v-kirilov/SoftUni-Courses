956c909521e7d09560a7a3103c378a94f35fa53e
v-kirilov <v_kirilov87@protonmail.ch>
2022-04-03 15:39:28 +0300

    Problem 04

---
+++ Fundamentals/Intro and Basic Syntax Lab/P04. Back in 30 Minutes/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace _4._Back_in_30_Minutes
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int hours = int.Parse(Console.ReadLine());
+            int minutes = int.Parse(Console.ReadLine());
+
+            if (hours ==24)
+            {
+                hours = 0;
+            }
+            if (minutes + 30 >= 60)
+            {
+                hours++;
+                if (hours==24)
+                {
+                    hours = 0;
+                    minutes = minutes + 30 - 60;
+                    Console.WriteLine($"{hours}:{minutes:d2}");
+                    return;
+                }
+                minutes = minutes + 30 - 60;
+                Console.WriteLine($"{hours}:{minutes:d2}");
+
+            
+            }else
+            {
+                Console.WriteLine($"{hours}:{minutes+30:d2}");
+            }
+
+        }
+    }
+}
