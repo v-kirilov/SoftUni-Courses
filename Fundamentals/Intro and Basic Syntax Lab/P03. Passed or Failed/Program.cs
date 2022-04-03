3ea17edf0689c61c40ab8f6b2dd70ddb3173d7bb
v-kirilov <v_kirilov87@protonmail.ch>
2022-04-03 15:39:16 +0300

    Problem 03

---
+++ Fundamentals/Intro and Basic Syntax Lab/P03. Passed or Failed/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace _2._Passed
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            float num = float.Parse(Console.ReadLine());
+            if (num >= 3)
+            {
+                Console.WriteLine("Passed!");
+            }else
+            {
+                Console.WriteLine("Failed!");
+            }
+        }
+    }
+}
