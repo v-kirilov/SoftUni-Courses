968aec6f42a955435f05c4f4ef6e38e912a979b5
v-kirilov <v_kirilov87@protonmail.ch>
2022-04-04 18:56:53 +0300

    Intro and Basic Syntax

---
+++ Fundamentals/Intro and Basic Syntax/Intro and Basic Syntax Lab/P08. Divisible by 3/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace _8._Divisible_by_3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {    //Това е 9та задача!!!!!!!!
+            int num = int.Parse(Console.ReadLine());
+            int sum = 0;
+            for (int i = 1; i <= num; i++)
+            {
+                Console.WriteLine(i+i-1);
+                sum += i + i - 1;
+            }
+            Console.WriteLine($"Sum: {sum}");
+        }
+    }
+}
---
+++ Fundamentals/Intro and Basic Syntax/Intro and Basic Syntax Lab/P11. Multiplication Table 2.0/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace _11._Multiplication_Table_2._0
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int num = int.Parse(Console.ReadLine());
+            int num2 = int.Parse(Console.ReadLine());
+
+            if (num<=10)
+            {
+                for (int i = num2; i <= 10; i++)
+                {
+                    Console.WriteLine($"{num} X {i} = {num*i}");
+
+                }
+            }else
+            {
+                    Console.WriteLine($"{num} X {num2} = {num* num2}");
+            }
+        }
+    }
+}
