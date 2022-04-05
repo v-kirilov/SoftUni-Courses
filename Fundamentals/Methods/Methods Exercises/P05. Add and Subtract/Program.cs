a84394e784ca55ecf0c51876c493b610d89bbb8e
v-kirilov <v_kirilov87@protonmail.ch>
2022-04-05 19:57:42 +0300

    Problem 05

---
+++ Fundamentals/Methods/Methods Exercises/P05. Add and Subtract/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace P05._Add_and_Subtract
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int num1 = int.Parse(Console.ReadLine());
+            int num2 = int.Parse(Console.ReadLine());
+            int num3 = int.Parse(Console.ReadLine());
+
+            int sum = SumMethod(num1, num2);
+            int diff = DifferenceMethod(sum, num3);
+            Console.WriteLine(diff);
+            //end
+        }
+
+        static int SumMethod(int num1, int num2)
+        {
+            int sum = num1 + num2;
+            return sum;
+        }
+        static int DifferenceMethod(int num1, int num2)
+        {
+            int diff = num1 - num2;
+            return diff;
+        }
+
+
+
+    }
+}
