21c203de5ff50edf621f2a058dfd06fac1bda81f
v-kirilov <v_kirilov87@protonmail.ch>
2022-04-05 19:57:00 +0300

    Problem 03

---
+++ Fundamentals/Methods/Methods Exercises/P03. Characters in Range/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace _3._Characters_in_Range
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            char first = char.Parse(Console.ReadLine());
+            char second = char.Parse(Console.ReadLine());
+
+            int start = first;
+            int end = second;
+            if (start<=end)
+            {
+                RangePlot(start, end);
+
+            }else
+            {
+                RangePlot(end, start);
+
+            }
+
+
+        }
+
+        static void RangePlot(int start, int end)
+        {
+            for (int i = start+1; i <= end-1; i++)
+            {
+                char letter = (char)i;
+                Console.Write($"{letter} ");
+            }
+        }
+    }
+}
