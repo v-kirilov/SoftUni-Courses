4fa6856d73d057bf47d34ff1bf148ee07ae001d4
v-kirilov <v_kirilov87@protonmail.ch>
2022-04-06 18:46:40 +0300

    Problem 06

---
+++ Fundamentals/Methods/Methods Exercises/P06. Middle Characters/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace P06._Middle_Characters
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+
+            Console.WriteLine(PrintMiddle(input));
+        }
+
+
+        static string PrintMiddle(string input)
+        {
+            string output = string.Empty;
+            if (input.Length % 2 != 0)
+            {
+                int middle = input.Length / 2;
+                output = input[middle].ToString();
+            }
+            else
+            {
+                int middle = input.Length / 2;
+                output=input[middle-1].ToString()+input[middle].ToString();
+            }
+
+            return output;
+        }
+
+
+    }
+}
