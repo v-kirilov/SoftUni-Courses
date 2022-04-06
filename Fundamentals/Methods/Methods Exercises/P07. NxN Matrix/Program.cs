68568e6be4bb29c0b6dd72204234d13fd5d91ee3
v-kirilov <v_kirilov87@protonmail.ch>
2022-04-06 18:46:52 +0300

    Problem 07

---
+++ Fundamentals/Methods/Methods Exercises/P07. NxN Matrix/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace P07._NxN_Matrix
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int num = int.Parse(Console.ReadLine());
+            MatrixMethod(num);
+        }
+
+
+        static void MatrixMethod(int num)
+        {
+            for (int i = 0; i < num; i++)
+            {
+                for (int j = 0; j < num; j++)
+                {
+                    Console.Write($"{num} ");
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
