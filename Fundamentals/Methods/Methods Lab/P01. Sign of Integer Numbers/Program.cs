c1e55d8b82af042dd400b5fb91882ef55d3652ce
v-kirilov <v_kirilov87@protonmail.ch>
2022-04-08 09:44:33 +0300

    Problem 01

---
+++ Fundamentals/Methods/Methods Lab/P01. Sign of Integer Numbers/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace _1._Sign_of_Integer_Numbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int num = int.Parse(Console.ReadLine());
+            string result = SignOfInteger(num);
+
+            Console.WriteLine(result);
+
+        }
+
+        static string SignOfInteger(int number)
+        {
+            string sign;
+
+            if (number > 0)
+            {
+                sign = "positive";
+            }
+            else if (number < 0)
+            {
+                sign = "negative";
+            }
+            else
+            {
+                sign = "zero";
+            }
+
+            return $"The number {number} is {sign}.";
+        }
+    }
+}
