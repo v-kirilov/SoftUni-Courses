a8c42a1ad74c06bfab2ba26bd37b36e858bd254b
v-kirilov <v_kirilov87@protonmail.ch>
2022-04-08 09:44:58 +0300

    Problem 02

---
+++ Fundamentals/Methods/Methods Lab/P02.Grades/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+
+namespace _2.Grades
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double newGrade = double.Parse(Console.ReadLine());
+            string gradeIs = GradeType(newGrade);
+
+            Console.WriteLine(gradeIs);
+
+        }
+
+        static string GradeType(double grade)
+        {
+            string corGrade;
+
+            if (grade>=2 && grade<=2.99 )
+            {
+                corGrade = "Fail";
+            }
+            else if (grade >=3 && grade <=3.49)
+            {
+                corGrade = "Poor";
+            }
+            else if (grade >= 3.5 && grade <= 4.49)
+            {
+                corGrade = "Good";
+            }
+            else if (grade >= 4.50 && grade <= 5.49)
+            {
+                corGrade = "Very good";
+            }
+            else if (grade >= 5.5 && grade <= 6)
+            {
+                corGrade = "Excellent";
+            }else
+            {
+                corGrade = "Invalid grade";
+
+            }
+            return $"{corGrade}";
+
+        }
+    }
+}
