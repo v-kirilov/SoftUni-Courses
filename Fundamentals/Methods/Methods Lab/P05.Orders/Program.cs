4ac31273ded784fed9087793ae9190a0ae062811
v-kirilov <v_kirilov87@protonmail.ch>
2022-04-09 17:21:34 +0300

    Problem 05

---
+++ Fundamentals/Methods/Methods Lab/P05.Orders/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace _5.Orders
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string product = Console.ReadLine();
+            int count = int.Parse(Console.ReadLine());
+            double endPrice = ProductOrder(product, count);
+            Console.WriteLine($"{endPrice:f2}");
+        }
+
+        static double ProductOrder(string product, int count)
+        {
+            double sum = 0;
+            switch (product)
+            {
+                case "coffee":
+                    sum = count * 1.5;
+                    break;
+                case "water":
+                    sum = count * 1;
+                    break;
+                case "coke":
+                    sum = count * 1.4;
+                    break;
+                case "snacks":
+                    sum = count * 2;
+                    break;
+            }
+
+            return sum;
+        }
+    }
+}
