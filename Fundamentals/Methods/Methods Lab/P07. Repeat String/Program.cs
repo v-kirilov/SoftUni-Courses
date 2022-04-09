6ad54365a8a5fbbcdf0ca53525653d854baecca2
v-kirilov <v_kirilov87@protonmail.ch>
2022-04-09 17:22:10 +0300

    Problem 07

---
+++ Fundamentals/Methods/Methods Lab/P07. Repeat String/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace _7._Repeat_String
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+
+            int num = int.Parse(Console.ReadLine());
+
+            Console.WriteLine(RepeatString(input, num));
+        }
+        static string RepeatString(string input, int count)
+        {
+            string output = null;
+            for (int i = 0; i < count; i++)
+            {
+                output += input;
+            }
+            return output;
+
+        }
+    }
+}
