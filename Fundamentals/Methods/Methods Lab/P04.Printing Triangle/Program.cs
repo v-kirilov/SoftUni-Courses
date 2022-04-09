e7eada2263a25a5566fcdb1b578d67c30f902abd
v-kirilov <v_kirilov87@protonmail.ch>
2022-04-09 17:21:10 +0300

    Problem 04

---
+++ Fundamentals/Methods/Methods Lab/P04.Printing Triangle/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace _4.Printing_Triangle
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int number = int.Parse(Console.ReadLine());
+            for (int i = 1; i <= number-1; i++)
+            {
+                StartTree(i);
+            }
+            StartTree(number); // Принтим тук средата
+            for (int j = number-1; j >0 ; j--)
+            {
+                StartTree(j);
+            }
+        }
+        static void StartTree(int num)
+        {
+            
+            for (int i = 1; i <= num; i++)
+            {
+                Console.Write($"{i} ");
+                
+            }
+            Console.WriteLine();
+        }
+    }
+}
