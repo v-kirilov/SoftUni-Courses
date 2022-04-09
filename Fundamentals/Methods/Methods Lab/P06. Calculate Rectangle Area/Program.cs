3b1646b7bdd64d0168b3fe9950292aa3518906a1
v-kirilov <v_kirilov87@protonmail.ch>
2022-04-09 17:21:52 +0300

    Problem 06

---
+++ Fundamentals/Methods/Methods Lab/P06. Calculate Rectangle Area/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace _6._Calculate_Rectangle_Area
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = int.Parse(Console.ReadLine());
+            int b = int.Parse(Console.ReadLine());
+            Console.WriteLine(SquareArea(a,b));
+        }
+        static int SquareArea(int num1, int num2)
+        {
+            int sum = num1 * num2;
+            return sum;
+        }
+    }
+}
