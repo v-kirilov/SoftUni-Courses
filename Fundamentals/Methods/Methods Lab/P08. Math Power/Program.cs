c38c722add733b0d272e070e03e0864e4b0586d6
v-kirilov <v_kirilov87@protonmail.ch>
2022-04-10 14:46:03 +0300

    Problem 08

---
+++ Fundamentals/Methods/Methods Lab/P08. Math Power/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace _8._Math_Power
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = int.Parse(Console.ReadLine());
+            int b = int.Parse(Console.ReadLine());
+            Console.WriteLine(Pow(a,b));
+
+        }
+        static double Pow(int n1, int n2)
+        {
+            double result = Math.Pow(n1, n2);
+            return result;
+        }
+    }
+}
