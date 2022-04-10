272b0b14375e6cd73041c89eb34a34525ca82340
v-kirilov <v_kirilov87@protonmail.ch>
2022-04-10 14:46:44 +0300

    Problem 11

---
+++ Fundamentals/Methods/Methods Lab/P11. Math Operations/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace _11._Math_Operations
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double num1 = double.Parse(Console.ReadLine());
+            string opera = Console.ReadLine();
+            double num2 = double.Parse(Console.ReadLine());
+            double sumOfElemens = CalculateTheNumbers(num1, opera, num2);
+            Console.WriteLine(sumOfElemens);
+
+        }
+
+        static double CalculateTheNumbers(double num1, string operating, double num2)
+        {
+            double sum = 0;
+            switch (operating)
+            {
+                case "+":
+                    sum = num1 + num2;
+                    break;
+                case "-":
+                    sum = num1 - num2;
+                    break;
+                case "*":
+                    sum = num1 * num2;
+                    break;
+                case "/":
+                    sum = num1 / num2;
+                    break;
+            }
+            return sum;
+        }
+    }
+}
