03a440ab043ed81c64f39d59d7aaf933f178517f
v-kirilov <v_kirilov87@protonmail.ch>
2022-04-11 20:09:30 +0300

    Problem 02

---
+++ Fundamentals/MidExam/Problem 1 - SoftUni Reception/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace Problem_1___SoftUni_Reception
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int empOne = int.Parse(Console.ReadLine());
+            int empTwo = int.Parse(Console.ReadLine());
+            int empThree = int.Parse(Console.ReadLine());
+            int students = int.Parse(Console.ReadLine());
+
+            if (students==0)
+            {
+                Console.WriteLine("Time needed: 0h.");
+                return;
+            }
+
+            double newNumber = Math.Ceiling(1.0 * students / (empOne + empThree + empTwo));
+            double newNumberTwo = Math.Floor(1.0 * students / (empOne + empThree + empTwo));
+            int addedTime = ((int)newNumberTwo / 3);
+            Console.WriteLine($"Time needed: {newNumber+addedTime}h.");
+        }
+    }
+}
