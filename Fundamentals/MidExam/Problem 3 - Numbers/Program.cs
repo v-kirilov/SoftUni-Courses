23b5f825881ca46a1091347d766905b3d711c381
v-kirilov <v_kirilov87@protonmail.ch>
2022-04-14 21:13:41 +0300

    Problem 3 - Numbers

---
+++ Fundamentals/MidExam/Problem 3 - Numbers/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Problem_3___Numbers
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> inputList = Console.ReadLine()
+                .Split(' ', StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse)
+                .ToList();
+
+            List<int> outputList = new List<int>();
+            double average = 0;
+            average = inputList.Average();
+
+            for (int i = 0; i < inputList.Count; i++)
+            {
+                if (inputList[i] > average)
+                {
+                    outputList.Add(inputList[i]);
+                }
+            }
+            outputList.Sort();
+            outputList.Reverse();
+            if (outputList.Count == 0)
+            {
+                Console.WriteLine("No");
+                return;
+            }
+            if (outputList.Count > 5)
+            {
+                int removeCount = outputList.Count - 5;
+                outputList.RemoveRange(5, removeCount);
+            }
+            Console.WriteLine(String.Join(" ", outputList));
+        }
+    }
+}
