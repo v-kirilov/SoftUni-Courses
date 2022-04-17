5a5ad77223a2bf27f6f748327a75aeed8c7af8cd
v-kirilov <v_kirilov87@protonmail.ch>
2022-04-17 11:17:49 +0300

    Problem 01

---
+++ Fundamentals/Text Processing/Text Processing - Lab/P01. Reverse Strings/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Linq;
+
+namespace P01._Reverse_Strings
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string word = Console.ReadLine();
+            while (word!="end")
+            {
+                char[] reversedWord = word.ToCharArray();
+                reversedWord = reversedWord.Reverse().ToArray();
+                string end = new string(reversedWord);
+
+                Console.WriteLine($"{word} = {end}");
+                word = Console.ReadLine();
+            }
+            
+        }
+    }
+}
