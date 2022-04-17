a13b3cad620ae6f1e50885186043bfaad37ff9ae
v-kirilov <v_kirilov87@protonmail.ch>
2022-04-17 11:18:04 +0300

    Problem 02

---
+++ Fundamentals/Text Processing/Text Processing - Lab/P02. Repeat Strings/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Text;
+
+namespace P02._Repeat_Strings
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] input = Console.ReadLine().Split(' ',StringSplitOptions.RemoveEmptyEntries);
+            StringBuilder str = new StringBuilder();
+
+            foreach (var item in input)
+            {
+                for (int i = 0; i < item.Length; i++)
+                {
+                    str.Append(item);
+                }
+            }
+            Console.WriteLine(str);
+
+        }
+    }
+}
