9a1e100f9e3fbc09d02635768c23842a217176f9
v-kirilov <v_kirilov87@protonmail.ch>
2022-04-18 17:11:45 +0300

    Problem - 04

---
+++ Fundamentals/Text Processing/Text Processing - Lab/P04. Text Filter/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace P04._Text_Filter
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] bannedWords = Console.ReadLine().Split(", ",StringSplitOptions.RemoveEmptyEntries);
+            string input = Console.ReadLine();
+
+            //for (int i = 0; i < bannedWords.Length; i++)
+            //{
+            //    string word = bannedWords[i];
+            //    string replace = string.Empty;
+            //    for (int j = 0; j < word.Length; j++)
+            //    {
+            //        replace += "*";
+            //    }
+            //    while (input.Contains(word))
+            //    {
+            //        int index = input.IndexOf(word);
+            //        input = input.Replace(word, replace);
+            //    }
+            //}
+            foreach (var word in bannedWords)
+            {
+                input = input.Replace(word, new string('*', word.Length));
+            }
+
+            Console.WriteLine(input);
+        }
+    }
+}
