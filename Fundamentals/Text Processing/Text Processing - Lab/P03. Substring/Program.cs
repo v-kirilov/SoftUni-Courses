34e63a139fe46a9952c60159df4d6c0df2b1a7ba
v-kirilov <v_kirilov87@protonmail.ch>
2022-04-18 17:11:29 +0300

    Problem - 03

---
+++ Fundamentals/Text Processing/Text Processing - Lab/P03. Substring/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace _3._Substring
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string remove = Console.ReadLine();
+            string objectsToRemove = Console.ReadLine();
+
+            while (objectsToRemove.Contains(remove))
+            {
+                int index = objectsToRemove.IndexOf(remove);
+                objectsToRemove = objectsToRemove.Remove(index,remove.Length);
+            }
+
+            Console.WriteLine(objectsToRemove);
+        }
+    }
+}
