2eb6fb8f422657ae54324f23a5bfd648a1472436
v-kirilov <v_kirilov87@protonmail.ch>
2022-04-18 17:11:58 +0300

    Problem - 05

---
+++ Fundamentals/Text Processing/Text Processing - Lab/P05. Digits, Letters, and Other/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Linq;
+using System.Text;
+
+namespace P05._Digits__Letters__and_Other
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+            char[] lettersFromInput = input.ToCharArray();
+
+            StringBuilder digits = new StringBuilder();
+            StringBuilder letters = new StringBuilder();
+            StringBuilder other = new StringBuilder();
+
+            for (int i = 0; i < lettersFromInput.Length; i++)
+            {
+                if (char.IsDigit(lettersFromInput[i]))
+                {
+                    digits.Append(lettersFromInput[i]);
+                }
+                else if (char.IsLetter(lettersFromInput[i]))
+                {
+                    letters.Append(lettersFromInput[i]);
+                }
+                else
+                {
+                    other.Append(lettersFromInput[i]);
+                }
+            }
+            Console.WriteLine(digits);
+            Console.WriteLine(letters);
+            Console.WriteLine(other);
+        }
+    }
+}
