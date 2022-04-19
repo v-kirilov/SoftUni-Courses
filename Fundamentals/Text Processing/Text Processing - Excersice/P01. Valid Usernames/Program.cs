858162703a5d27be9f7397909e6105fef9c23a55
v-kirilov <v_kirilov87@protonmail.ch>
2022-04-19 20:41:13 +0300

    Problem 01

---
+++ Fundamentals/Text Processing/Text Processing - Excersice/P01. Valid Usernames/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace P01._Valid_Usernames
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] passwords = Console.ReadLine().Split(", ", StringSplitOptions.RemoveEmptyEntries);
+            foreach (var word in passwords)
+            {
+                bool isValid = true;
+
+                if (word.Length < 3 || word.Length > 16)
+                {
+                    continue;
+                }
+                foreach (var letter in word)
+                {
+                    if (char.IsLetterOrDigit(letter) || letter == '_' || letter == '-')
+                    {
+                        continue;
+                    }
+                    else
+                    {
+                        isValid = false;
+                    }
+                }
+
+                if (isValid)
+                {
+                    Console.WriteLine(word);
+                }
+            }
+        }
+    }
+}
