81da6b53e0b73c239e96819eeef9720443cdc7ef
v-kirilov <v_kirilov87@protonmail.ch>
2022-04-20 19:12:50 +0300

    Problem 04

---
+++ Fundamentals/Text Processing/Text Processing - Excersice/P04.  Caesar Cipher/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Text;
+
+namespace P04.__Caesar_Cipher
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+            StringBuilder str = new StringBuilder();
+
+            foreach (var character in input)
+            {
+                int num = (int)character + 3;
+                char ch = (char)num;
+
+                str.Append(ch);
+            }
+
+            Console.WriteLine(str.ToString());
+        }
+    }
+}
