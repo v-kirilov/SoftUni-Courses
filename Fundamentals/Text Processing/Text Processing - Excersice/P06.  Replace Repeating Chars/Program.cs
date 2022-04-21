be0d5e686fc336ab0014f856f83b23841faea369
v-kirilov <v_kirilov87@protonmail.ch>
2022-04-21 20:35:30 +0300

    Problem 06

---
+++ Fundamentals/Text Processing/Text Processing - Excersice/P06.  Replace Repeating Chars/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Text;
+
+namespace P06.__Replace_Repeating_Chars
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+
+            StringBuilder str = new StringBuilder();
+
+            for (int i = 0; i < input.Length; i++)
+            {
+                if (i+1>=input.Length)
+                {
+                    str.Append(input[i]);
+                    break;
+                }
+                if (input[i+1]==input[i])
+                {
+                    continue;
+                }else
+                {
+                    str.Append(input[i]);
+                }
+            }
+
+            Console.WriteLine(str.ToString());
+        }
+    }
+}
