6df1506bc8ef9124c0a420d17e1c37ed6a17d1a1
v-kirilov <v_kirilov87@protonmail.ch>
2022-04-21 20:09:00 +0300

    Problem 05

---
+++ Fundamentals/Text Processing/Text Processing - Excersice/P05.  Multiply Big Number/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace P05.__Multiply_Big_Number
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            decimal first = decimal.Parse(Console.ReadLine());
+            double second = double.Parse(Console.ReadLine());
+
+            Console.WriteLine(first * (decimal)second);
+
+        }
+    }
+}
