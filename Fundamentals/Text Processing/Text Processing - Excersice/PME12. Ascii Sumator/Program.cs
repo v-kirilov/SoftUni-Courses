1c9489f7749c72ab83b7c8ca0b183f2494b1e421
v-kirilov <v_kirilov87@protonmail.ch>
2022-04-22 10:54:17 +0300

    Problem 12

---
+++ Fundamentals/Text Processing/Text Processing - Excersice/PME12. Ascii Sumator/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace PME12._Ascii_Sumator
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            char start = char.Parse(Console.ReadLine());
+            char end = char.Parse(Console.ReadLine());
+            
+
+            string input = Console.ReadLine();
+            int startIndex = 0;
+            int endIndex = input.Length;
+            if (input.Contains(start))
+            {
+                startIndex = input.IndexOf(start);
+            }
+            if (input.Contains(end))
+            {
+                endIndex = input.IndexOf(end);
+            }
+
+            string betweenChars = input.Substring(startIndex, endIndex);
+            int sum = 0;
+            foreach (var character in betweenChars)
+            {   
+                sum += (int)character;
+            }
+
+            Console.WriteLine(sum);
+        }
+    }
+}
