eb4118e1ffedd0c98b6cde0d8098f89c39be45d7
v-kirilov <v_kirilov87@protonmail.ch>
2022-04-22 10:53:48 +0300

    Problem 08

---
+++ Fundamentals/Text Processing/Text Processing - Excersice/P08. Letters Change Numbers/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+
+namespace P08._Letters_Change_Numbers
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] input = Console.ReadLine().Split(' ',StringSplitOptions.RemoveEmptyEntries);
+            double sum = 0;
+            double totalSum = 0;
+
+            foreach (var word in input)
+            {
+                string number = string.Empty;
+                sum = 0;
+                for (int i = 0; i < word.Length; i++)
+                {
+                    if (char.IsDigit(word[i]))
+                    {
+                        number += word[i];
+                    }
+                }
+                sum += double.Parse(number);
+
+                if (char.IsUpper(word[0]))
+                {
+                    sum /= ((int)word[0] - 64);
+                }
+                else
+                {
+                    sum *= ((int)word[0] - 96);
+                }
+
+                if (char.IsUpper(word[word.Length-1]))
+                {
+                    sum -= (int)word[word.Length - 1] - 64;
+                }
+                else
+                {
+                    sum += ((int)word[word.Length - 1] - 96);
+                }
+                totalSum += sum;
+            }
+
+            Console.WriteLine($"{totalSum:f2}");
+        }
+    }
+}
