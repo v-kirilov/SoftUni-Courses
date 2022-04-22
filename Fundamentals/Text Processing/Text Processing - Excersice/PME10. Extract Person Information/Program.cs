29e29016c63e1cde18a5d3bd5dacbfb69142636f
v-kirilov <v_kirilov87@protonmail.ch>
2022-04-22 10:54:05 +0300

    Problem 10

---
+++ Fundamentals/Text Processing/Text Processing - Excersice/PME10. Extract Person Information/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace PME10._Extract_Person_Information
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < n; i++)
+            {
+                string example = Console.ReadLine();
+                int nameStart = example.IndexOf('@');
+                int nameEnd = example.IndexOf('|');
+                string name = example.Substring(nameStart+1, nameEnd - nameStart-1);
+
+
+                int ageStart = example.IndexOf('#');
+                int ageEnd = example.IndexOf('*');
+                string age = example.Substring(ageStart+1, ageEnd - ageStart-1);
+
+                Console.WriteLine($"{name} is {age} years old.");
+            }
+        }
+    }
+}
