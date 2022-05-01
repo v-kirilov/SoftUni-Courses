9186835b252aca8cb03eb1bb766719266d805ab3
v-kirilov <v_kirilov87@protonmail.ch>
2022-05-01 17:13:00 +0300

    Problem 02

---
+++ Fundamentals/Lists/Lists-Lab/P02. Gauss' Trick/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace P02._Gauss__Trick
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<double> numbers = Console.ReadLine()
+                .Split(" ")
+                .Select(double.Parse)
+                .ToList();
+
+            List<double> resultList = new List<double>();
+
+
+            for (int i = 0; i < numbers.Count / 2; i++)
+            {
+                double sum = numbers[i] + numbers[numbers.Count - i - 1];
+                resultList.Add(sum);
+
+
+            }
+
+            if (numbers.Count % 2 != 0)
+            {
+                resultList.Add(numbers[numbers.Count /2]);
+            }
+
+
+            Console.WriteLine(String.Join(" ", resultList));
+        }
+    }
+}
