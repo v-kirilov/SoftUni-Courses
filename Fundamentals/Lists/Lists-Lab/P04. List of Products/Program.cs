cd75f9c271b16ba1c4705711e6a8d1d973fb34a3
v-kirilov <v_kirilov87@protonmail.ch>
2022-05-02 12:17:33 +0300

    Problem 04

---
+++ Fundamentals/Lists/Lists-Lab/P04. List of Products/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace P04._List_of_Products
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int number = int.Parse(Console.ReadLine());
+
+            List<string> product = new List<string>(number);
+
+            for (int i = 0; i < number; i++)
+            {
+                product.Add(Console.ReadLine());
+            }
+
+            product.Sort();
+
+            for (int i = 0; i < product.Count; i++)
+            {
+                Console.WriteLine($"{i + 1}.{product[i]}");
+            }
+            //int counter = 1;
+            //foreach (string item in product)
+            //{
+
+            //    Console.WriteLine($"{counter}.{item}");
+            //    counter++;
+            //}
+        }
+    }
+}
