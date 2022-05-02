9310de8eafb77320c4e16ecfdf47288bde5fd7f4
v-kirilov <v_kirilov87@protonmail.ch>
2022-05-02 12:17:21 +0300

    Problem 03

---
+++ Fundamentals/Lists/Lists-Lab/P03. Merging Lists/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace P03._Merging_Lists
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<double> firstList = Console.ReadLine()
+                .Split(" ")
+                .Select(double.Parse)
+                .ToList();
+
+            List<double> secondList = Console.ReadLine()
+                .Split(" ")
+                .Select(double.Parse)
+                .ToList();
+
+            List<double> resultList = new List<double>();
+
+            int bigCount = 0;
+
+            if (firstList.Count > secondList.Count)
+            {
+                bigCount = firstList.Count;
+            }
+            else
+            {
+                bigCount = secondList.Count;
+            }
+
+            for (int i = 0; i < bigCount; i++)
+            {
+                if (firstList.Count >= i+1)
+                {
+                    resultList.Add(firstList[i]);
+                }
+                if (secondList.Count >= i+1)
+                {
+                    resultList.Add(secondList[i]);
+                }
+            }
+
+            Console.WriteLine(string.Join(" ", resultList));
+        }
+    }
+}
