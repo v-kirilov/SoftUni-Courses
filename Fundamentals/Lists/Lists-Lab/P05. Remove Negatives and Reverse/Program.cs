c4aed41079fc35bd068bca5104fce534454b4de6
v-kirilov <v_kirilov87@protonmail.ch>
2022-05-03 20:36:40 +0300

    Problem 05

---
+++ Fundamentals/Lists/Lists-Lab/P05. Remove Negatives and Reverse/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace P05._Remove_Negatives_and_Reverse
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> numberList = Console.ReadLine()
+                .Split(" ")
+                .Select(int.Parse)
+                .ToList();
+
+            numberList.RemoveAll(x => x < 0);
+
+
+            numberList.Reverse();
+
+            if (numberList.Count>0)
+            {
+                Console.WriteLine(String.Join(" ", numberList));
+            }else
+            {
+                Console.WriteLine("empty");
+            }
+        }
+    }
+}
