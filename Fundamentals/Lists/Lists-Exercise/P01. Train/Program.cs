e3720713b8d39b9f29eca15b81c2f6ec2b6be551
v-kirilov <v_kirilov87@protonmail.ch>
2022-05-04 19:16:18 +0300

    Problem 01

---
+++ Fundamentals/Lists/Lists-Exercise/P01. Train/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace P01._Train
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> vagons = Console.ReadLine()
+                .Split(" ", StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse)
+                .ToList();
+            int maxCapacity = int.Parse(Console.ReadLine());
+
+            string input = string.Empty;
+
+            while ((input = Console.ReadLine()) != "end")
+            {
+                string[] inputArray = input.Split(" ", StringSplitOptions.RemoveEmptyEntries);
+
+
+                if (inputArray[0] == "Add")
+                {
+                    int newWagon = int.Parse(inputArray[1]);
+                    vagons.Add(newWagon);
+                }
+                else
+                {
+                    int extraPassengers = int.Parse(inputArray[0]);
+                    for (int i = 0; i < vagons.Count; i++)
+                    {
+                        int currentVagon = vagons[i];
+                        if (extraPassengers+currentVagon>maxCapacity)
+                        {
+                            continue;
+                        }else
+                        {
+                            vagons[i]+=extraPassengers;
+                            break;
+                        }
+                    }
+                }
+            }
+            Console.WriteLine(String.Join(" ",vagons));
+        }
+    }
+}
