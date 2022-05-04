2e835d1e0755f2c9684f7d66387c7a7772fad359
v-kirilov <v_kirilov87@protonmail.ch>
2022-05-04 19:16:31 +0300

    Problem 02

---
+++ Fundamentals/Lists/Lists-Exercise/P02. Change List/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace P02._Change_List
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> inputList = Console.ReadLine()
+                .Split(" ", StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse)
+                .ToList();
+
+            string action = string.Empty;
+
+            while ((action = Console.ReadLine()) != "end")
+            {
+                string[] inputArr = action.Split(" ", StringSplitOptions.RemoveEmptyEntries).ToArray();
+
+                string firstCommand = inputArr[0];
+                if (firstCommand == "Delete")
+                {
+                    int element = int.Parse(inputArr[1]);
+                    inputList.RemoveAll(x => x == element);
+                }
+                else
+                {
+                    int element = int.Parse(inputArr[1]);
+                    int position = int.Parse(inputArr[2]);
+                    inputList.Insert(position, element);
+                }
+
+            }
+
+            Console.WriteLine(String.Join(" ", inputList));
+        }
+    }
+}
