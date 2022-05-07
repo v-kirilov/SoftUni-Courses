c9c107c6025f3c398bed3d0c35880d3f8528f4cb
v-kirilov <v_kirilov87@protonmail.ch>
2022-05-07 14:46:44 +0300

    Problem 07

---
+++ Fundamentals/Lists/Lists-Exercise/P07. Append Arrays/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace P07._Append_Arrays
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] input = Console.ReadLine()
+                .Split('|', StringSplitOptions.RemoveEmptyEntries);
+
+            List<int> finalProduct = new List<int>();
+
+            for (int i = input.Length-1; i >= 0; i--)
+            {
+                List<int> temp = input[i]
+                    .Split(" ", StringSplitOptions.RemoveEmptyEntries)
+                    .Select(int.Parse)
+                    .ToList();
+
+                for (int j = 0; j < temp.Count; j++)
+                {
+                    int newNumber = temp[j];
+                    finalProduct.Add(newNumber);
+                }
+            }
+
+            Console.WriteLine(String.Join(" ", finalProduct));
+        }
+    }
+}
