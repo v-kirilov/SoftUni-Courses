3de40aa2d84e4f296afd10c9a4b8d54976dee47d
v-kirilov <v_kirilov87@protonmail.ch>
2022-05-09 18:43:19 +0300

    Problem 01

---
+++ Fundamentals/RegularExpressions/Regular Expresions Lab/P01. Match Full Name/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+namespace P01._Match_Full_Name
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string regex = @"\b[A-Z][a-z]{1,} [A-Z][a-z]{1,}\b";
+            string names = Console.ReadLine();
+
+            MatchCollection matchedNames = Regex.Matches(names, regex);
+
+            foreach (Match m in matchedNames)
+            {
+                Console.Write(m.Value +" ");
+            }
+        }
+    }
+}
