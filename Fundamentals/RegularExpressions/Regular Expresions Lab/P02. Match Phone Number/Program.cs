adfb6dd0ba9d3d02801db4bdb38584a5bb2ff0be
v-kirilov <v_kirilov87@protonmail.ch>
2022-05-10 21:37:07 +0300

    Problem 02

---
+++ Fundamentals/RegularExpressions/Regular Expresions Lab/P02. Match Phone Number/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+namespace _2._Match_Phone_Number
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string regex = @"\+359+( |-)2\1+\d{3}\1+\d{4}\b";
+            string input = Console.ReadLine();
+
+            MatchCollection matches = Regex.Matches(input, regex);
+
+            Console.Write(string.Join(", ", matches));
+
+            
+
+        }
+    }
+}
