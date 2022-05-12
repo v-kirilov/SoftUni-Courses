c4f9ec615dca9081bc5bde696b0ec6de6e1b17ce
v-kirilov <v_kirilov87@protonmail.ch>
2022-05-12 18:46:53 +0300

    Problem 01

---
+++ Fundamentals/RegularExpressions/Regular Expresions Exercise/P01. Furniture/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text.RegularExpressions;
+
+namespace P01._Furniture
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string pattern = @"[>]{2}(?<product>[A-Za-z]+)(?:<<)(?<price>\d+\.*\d+)!(?<quantity>\d{1,})";
+
+            List<string> furniture = new List<string>();
+            decimal price = 0m;
+            int qty = 0;
+            decimal totalSpend = 0;
+
+            string input = Console.ReadLine();
+            while (input!= "Purchase")
+            {
+                Match order = Regex.Match(input, pattern);
+
+                if (order.Success)
+                {
+                    furniture.Add(order.Groups["product"].Value);
+                    price = decimal.Parse(order.Groups["price"].Value);
+                    qty = int.Parse(order.Groups["quantity"].Value);
+                    totalSpend += price * qty;
+
+                }
+
+                input = Console.ReadLine();
+            }
+
+            Console.WriteLine("Bought furniture:");
+            foreach (var item in furniture)
+            {
+                Console.WriteLine(item);
+            }
+            Console.WriteLine($"Total money spend: {totalSpend:f2}");
+
+           
+        }
+    }
+}
