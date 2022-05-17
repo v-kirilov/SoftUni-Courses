815f54eb8f6abab221db4208df945549ee77148c
v-kirilov <v_kirilov87@protonmail.ch>
2022-05-17 18:45:38 +0300

    Problem 06

---
+++ Fundamentals/RegularExpressions/Regular Expresions Exercise/P06. Extract Emails/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+namespace P06._Extract_Emails
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Regex pattern = new Regex(@"(?<=\s|^)[a-z]+[-_.,]?[a-z]+@[a-z]+[-,_.]*[a-z]+[.,-_][a-z]+.?[a-z]+"); 
+            string input = Console.ReadLine();
+
+            MatchCollection matches = pattern.Matches(input);
+
+            foreach (Match match in matches)
+            {
+                Console.WriteLine(match);
+            }
+        }
+    }
+}
