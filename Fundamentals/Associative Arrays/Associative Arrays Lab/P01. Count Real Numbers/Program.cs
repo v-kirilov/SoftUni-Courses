82ca03f992e8ab0a43f8b6c7399fb4d23265ec8e
v-kirilov <v_kirilov87@protonmail.ch>
2022-05-21 10:56:05 +0300

    Problem 01

---
+++ Fundamentals/Associative Arrays/Associative Arrays Lab/P01. Count Real Numbers/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace P01._Count_Real_Numbers
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double[] input = Console.ReadLine()
+                .Split(' ',StringSplitOptions.RemoveEmptyEntries)
+                .Select(double.Parse)
+                .ToArray();
+
+            SortedDictionary<double,int> dict=new SortedDictionary<double,int>();
+
+            foreach (double item in input)
+            {
+                if (dict.ContainsKey(item))
+                {
+                    dict[item]++;
+                }else
+                {
+                    dict.Add(item, 1);
+                }
+            }
+
+            foreach (KeyValuePair<double,int> item in dict)
+            {
+                Console.WriteLine($"{item.Key} -> {item.Value}");
+            }
+        }
+    }
+}
