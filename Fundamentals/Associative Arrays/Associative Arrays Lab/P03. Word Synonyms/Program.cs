af0059d7504fbc3defc4e638b0278972dd0e9a44
v-kirilov <v_kirilov87@protonmail.ch>
2022-05-22 12:23:01 +0300

    Problem 03

---
+++ Fundamentals/Associative Arrays/Associative Arrays Lab/P03. Word Synonyms/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+
+namespace P03._Word_Synonyms
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+
+            Dictionary<string, List<string>> dict = new Dictionary<string, List<string>>();
+
+            for (int i = 0; i < n; i++)
+            {
+                string word = Console.ReadLine();
+                string synonym = Console.ReadLine();
+
+
+                if (dict.ContainsKey(word) == false)
+                {
+                    dict.Add(word, new List<string>() { synonym });
+                }
+                else
+                {
+                    dict[word].Add(synonym);
+                }
+
+            }
+
+            foreach (var item in dict)
+            {
+                Console.WriteLine($"{item.Key} - {string.Join(", ", item.Value)}");
+            }
+        }
+    }
+}
