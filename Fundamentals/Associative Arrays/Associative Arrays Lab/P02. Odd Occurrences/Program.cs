cdacad8eb6b4c39423976e74e0b009efc7931e43
v-kirilov <v_kirilov87@protonmail.ch>
2022-05-22 12:22:47 +0300

    Problem 02

---
+++ Fundamentals/Associative Arrays/Associative Arrays Lab/P02. Odd Occurrences/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace P02._Odd_Occurrences
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] input = Console.ReadLine()
+                .ToLower()
+                .Split(' ', StringSplitOptions.RemoveEmptyEntries);
+
+            Dictionary<string, int> dict = new Dictionary<string, int>();
+
+            foreach (var item in input)
+            {
+                if (dict.ContainsKey(item))
+                {
+                    dict[item]++;
+                }
+                else
+                {
+                    dict.Add(item, 1);
+                }
+            }
+
+            foreach (var word in dict)
+            {
+                if (word.Value % 2!=0)
+                {
+                    Console.Write(word.Key + " ");
+                }
+            }
+
+        }
+    }
+}
