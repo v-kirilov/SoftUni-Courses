6c5c8eb39f7bc265be1d623f3f787e02324561b7
v-kirilov <v_kirilov87@protonmail.ch>
2022-05-23 20:41:09 +0300

    Project 04

---
+++ Fundamentals/Associative Arrays/Associative Arrays Lab/P04. Word Filter/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Linq;
+
+namespace P04._Word_Filter
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] words = Console.ReadLine()
+                .Split(' ',StringSplitOptions.RemoveEmptyEntries)
+                .Where(x=>x.Length%2==0)
+                .ToArray();
+
+            foreach (var item in words)
+            {
+                Console.WriteLine(item);
+            }
+        }
+    }
+}
