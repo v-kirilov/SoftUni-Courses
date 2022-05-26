fc5393479e11660104f878331edff821990fe7a4
v-kirilov <v_kirilov87@protonmail.ch>
2022-05-26 23:58:29 +0300

    Problem 02

---
+++ Fundamentals/Associative Arrays/Exercise Associative Arrays/P02. A Miner Task/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+
+namespace P02._A_Miner_Task
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string resource = Console.ReadLine();
+            Dictionary<string,int> dict = new Dictionary<string,int>();
+
+            while (resource!="stop")
+            {
+                int quantity = int.Parse(Console.ReadLine());
+                
+                    if (dict.ContainsKey(resource) ==false)
+                    {
+                        dict.Add(resource,quantity);
+                    }else
+                    {
+                        dict[resource] += quantity;
+                    }
+               
+                resource = Console.ReadLine();
+            }
+
+            foreach (var item in dict)
+            {
+                Console.WriteLine($"{item.Key} -> {item.Value}");
+            }
+        }
+    }
+}
