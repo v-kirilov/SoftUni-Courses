18ce8041b5351ad289f6fdb91c1b2615f2e8693f
v-kirilov <v_kirilov87@protonmail.ch>
2022-05-26 23:58:12 +0300

    Problem 01

---
+++ Fundamentals/Associative Arrays/Exercise Associative Arrays/P01. Count Chars in a String/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+
+namespace P01._Count_Chars_in_a_String
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+
+            Dictionary<string, int> dict = new Dictionary<string, int>();
+
+            for (int i = 0; i < input.Length; i++)
+            {
+                string data = input[i].ToString();
+                if (data==" ")
+                {
+                    continue;
+                }
+                if (dict.ContainsKey(data)==false)
+                {
+                    dict.Add(data, 1);
+                }else
+                {
+                    dict[data]++;
+                }
+            }
+
+            foreach (var item in dict)
+            {
+                Console.WriteLine($"{item.Key} -> {item.Value}");
+            }
+        }
+    }
+}
