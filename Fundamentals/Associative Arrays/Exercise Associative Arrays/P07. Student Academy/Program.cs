5b1476301a4d85181f1c5421ca15295c8ccc3dfa
v-kirilov <v_kirilov87@protonmail.ch>
2022-05-30 23:14:15 +0300

    Problem 07

---
+++ Fundamentals/Associative Arrays/Exercise Associative Arrays/P07. Student Academy/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+
+namespace P07._Student_Academy
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Dictionary<string, List<double>> dict = new Dictionary<string, List<double>>();
+            int n = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < n; i++)
+            {
+                string name = Console.ReadLine();
+                double grade = double.Parse(Console.ReadLine());
+
+                if (!dict.ContainsKey(name))
+                {
+                    List<double> newList = new List<double>() { grade };
+                    dict.Add(name, newList);
+                }
+                else
+                {
+                    dict[name].Add(grade);
+                }
+            }
+
+            Dictionary<string,double> finalDict= new Dictionary<string,double>();
+
+            foreach (var item in dict)
+            {
+                if (item.Value.Average()>=4.5)
+                {
+                    finalDict.Add(item.Key, item.Value.Average());
+                }
+            }
+
+            foreach (var item in finalDict)
+            {
+                Console.WriteLine($"{item.Key} -> {item.Value:f2}");
+            }
+        }
+    }
+}
