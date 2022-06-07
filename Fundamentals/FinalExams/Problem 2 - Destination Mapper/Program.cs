4b51ce6ef2532791963c9591d166ba9099d9411f
v-kirilov <v_kirilov87@protonmail.ch>
2022-06-07 10:53:25 +0300

    Problem 2 - Destination Mapper

---
+++ Fundamentals/FinalExams/Problem 2 - Destination Mapper/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Text.RegularExpressions;
+
+namespace Problem_2___Destination_Mapper
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Regex pattern = new Regex(@"(=|\/)(?<places>[A-Z][a-zA-z]{2,})(\1)");
+
+            string places = Console.ReadLine();
+
+            MatchCollection matches = pattern.Matches(places);
+
+            int travelPoints = 0;
+            List<string> placesList = new List<string>();
+
+            foreach (Match match in matches)
+            {
+                travelPoints += match.Groups["places"].Value.Length;
+                placesList.Add(match.Groups["places"].Value);
+            }
+
+            Console.WriteLine($"Destinations: {string.Join(", ",placesList)}");
+            Console.WriteLine($"Travel Points: {travelPoints}");
+        }
+    }
+
+
+}
+
