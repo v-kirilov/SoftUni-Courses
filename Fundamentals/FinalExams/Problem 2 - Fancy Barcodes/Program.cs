84b467b90f0ee261f8df9604ee9212c732006e40
v-kirilov <v_kirilov87@protonmail.ch>
2022-06-09 00:17:46 +0300

    Problem 2 - Fancy Barcodes

---
+++ Fundamentals/FinalExams/Problem 2 - Fancy Barcodes/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Text.RegularExpressions;
+using System.Linq;
+
+namespace Problem_2___Fancy_Barcodes
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Regex pattern = new Regex(@"(@#+)(?<text>[A-Z][a-zA-Z\d]{4,}[A-Z])@#+");
+
+            int number = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < number; i++)
+            {
+                string input = Console.ReadLine();
+
+                if (pattern.IsMatch(input))
+                {
+                    char[] digits = input.Where(x => char.IsDigit(x)).ToArray();
+
+                    if (digits.Length == 0)
+                    {
+                        Console.WriteLine($"Product group: 00");
+                    }
+                    else
+                    {
+                        Console.WriteLine($"Product group: {string.Join("", digits)}");
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("Invalid barcode");
+                }
+            }
+
+        }
+    }
+}
