b2df2de5fcfdda3be202fdc935911471cb8ed018
v-kirilov <v_kirilov87@protonmail.ch>
2022-06-19 20:52:06 +0300

    Telerik Mock Exam 3

---
+++ Fundamentals/FinalExams/Telerik Mock 3/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Telerik_Mock_3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string anagramOriginal = Console.ReadLine();
+            int num = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < num; i++)
+            {
+                string anagram = anagramOriginal;
+                string input = Console.ReadLine();
+                if (anagram.Length != input.Length)
+                {
+                    Console.WriteLine("No");
+                    continue;
+                }
+                char[] anaChar = anagram.ToLower().ToCharArray();
+                char[] inputChar = input.ToLower().ToCharArray();
+
+                Array.Sort(inputChar);
+                Array.Sort(anaChar);
+                bool isAnagram = true;
+
+                for (int j = 0; j < anaChar.Length; j++)
+                {
+                    if (anaChar[j] != inputChar[j])
+                    {
+                        Console.WriteLine("No");
+                        isAnagram = false;
+                        break;
+                    }
+                }
+                if (isAnagram)
+                {
+                    Console.WriteLine("Yes");
+                }
+            }
+        }
+    }
+}
