599cf8d9049d5c6a443c4b3532e573b7ff60474f
v-kirilov <v_kirilov87@protonmail.ch>
2022-06-23 11:52:12 +0300

    Exam 2

---
+++ Fundamentals/FinalExams/Exam-2/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Numerics;
+using System.Text.RegularExpressions;
+
+namespace Exam_2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Regex pattern = new Regex(@"(@|#)+(?<color>[a-z]{3,})(@|#)+([^a-zA-Z\d]*)(\/)+(?<amount>\d+)(\/)+");
+            string input = Console.ReadLine();
+
+            MatchCollection matches = pattern.Matches(input);
+
+            foreach (Match match in matches)
+            {
+                decimal amount = decimal.Parse(match.Groups["amount"].Value);
+                
+              
+                Console.WriteLine($"You found {amount} {match.Groups["color"].Value} eggs!");
+                
+            }
+        }
+    }
+}
